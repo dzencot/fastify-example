481fbd9cedc2c727f9d669017752dc02c15e6ebf
Ivan Gagarinov <dzencot@gmail.com>
2022-11-08 00:11:22 +0500

    add react app

--- app.js
+++ app.js
@@ -2,9 +2,18 @@
 
 const path = require('path')
 const AutoLoad = require('@fastify/autoload')
+const fastifyStatic = require('@fastify/static');
 
 module.exports = async function (fastify, opts) {
   // Place here your custom code!
+
+  fastify.register(fastifyStatic, {
+    root: `${process.cwd()}/my-app/build`,
+  });
+
+  fastify.setNotFoundHandler((req, res) => {
+    res.sendFile('index.html');
+  });
 
   // Do not touch the following lines
 
